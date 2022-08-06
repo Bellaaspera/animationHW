791998186e784dfea271c1c5a1a45d3d64dd0e86
Bellaaspera <lanasenatorova@gmail.com>
2022-08-06 11:50:36 +0300

    Interfase

--- animationHW/ViewController.swift
+++ animationHW/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var viewLabel: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
 }
 
